3d604d79a831b23ef0f7f5ae33f16fdf2811cfae
luke-zhu <luke_zhu@brown.edu>
2016-11-20 03:19:14 -0500

    Final show similarities

--- data-analysis.py
+++ data-analysis.py
@@ -15,28 +15,43 @@ if __name__ == '__main__':
     }
     sorted_df = grouped.agg(aggs).score \
         .sort_values('average_score', ascending=False)
-
-    # Get P(A and B)/P(A)/P(B) for each pair of shows (A, B)
     
     users = filtered.groupby('user')
-
-    show_similarity = pd.DataFrame(index=sorted_df.index,
-        columns=sorted_df.index)
-
-    pairs = pd.merge(filtered[['user', 'title']], filtered[['user', 'title']],
+    """
+    pairs = pd.merge(filtered[['user', 'title']],
+        filtered[['user', 'title']],
         how='inner',
         on=['user'])
 
-    pair_counts= pairs.groupby(['title_x', 'title_y']).count()
-    """
-    for title1, group1 in grouped:
-        print(title1)
-        num_t1 = len(group1)
-        for title2, group2 in grouped:
-            num_t2 = len(group2)
-            num_both = len(pd.merge(group1, group2, how='inner', on=['user']))
-            show_similarity[title1][title2] = num_both ** 2 / (num_t1 * num_t2)
-            # print('{}, {}'.format(title1, title2))
-    """
+    def jaccard(group):
+        union_size = len(sorted_df.loc[group.title_x]['num_scores']) + \
+            len(sorted_df.loc[group.title_y]['num_scores']) - \
+            group.count()
+        return group['user'].count() / union_size
 
-    show_similarity.to_json('show-similarity-1.json')
+    similarities = pairs.groupby(['title_x', 'title_y']).apply(jaccard)
+    """
+    """
+    for i in range(0, 81):
+        print(i)
+        pair_counts = pd.read_csv('pair-counts/pair-counts-{}'.format(i))
+        similarities = pair_counts.apply(
+            lambda row: row['user'] / \
+                (sorted_df.loc[row.title_x]['num_scores'] + \
+                sorted_df.loc[row.title_y]['num_scores'] - \
+                row['user']),
+            axis=1)
+        similarities.to_csv('similarities/similarities-{}.csv'.format(i))
+    """
+    """
+    for i in range(0, 81):
+        print(i)
+        pair_counts = pd.read_csv('pair-counts/pair-counts-{}'.format(i))
+        similarities = pd.read_csv('similarities/similarities-{}.csv'.format(i),
+            header=None)
+        final_df = pair_counts.join(similarities) \
+            .set_index(['title_x', 'title_y']) \
+            .rename(columns={'user': 'count', 1: 'similarity'}) \
+            .drop([0], axis=1)
+        final_df.to_csv('final-dfs/final-df-{}.csv'.format(i))
+    """
